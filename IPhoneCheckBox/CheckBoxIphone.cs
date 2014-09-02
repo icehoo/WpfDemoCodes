996fea07f67a421ecc4bcf4dfd0ff9b8e3297b86
ZoeCheck <893622396@qq.com>
2014-09-02 22:30:06 +0800

    添加了仿IPhone的CheckBox样式

---
+++ IPhoneCheckBox/CheckBoxIphone.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace IPhoneCheckBox
+{
+	class CheckBoxIphone : CheckBox
+	{
+		Storyboard sbMoveToRight;
+		Storyboard sbMoveToLeft;
+		Storyboard sbRectangleShow;
+		Storyboard sbRectangleHide;
+		Ellipse ellipse;
+		Path rectangle;
+
+		public CheckBoxIphone()
+		{
+			Style styleCheckBox = this.FindResource("CheckBoxStyleIP") as Style;
+			if (styleCheckBox != null)
+			{
+				this.Style = styleCheckBox;
+			}
+
+			sbMoveToRight = this.FindResource("SB_EllipseMoveToRight") as Storyboard;
+			sbMoveToLeft = this.FindResource("SB_EllipseMoveToLeft") as Storyboard;
+			sbRectangleShow = this.FindResource("SB_RectangleShow") as Storyboard;
+			sbRectangleHide = this.FindResource("SB_RectangleHide") as Storyboard;
+		}
+
+		protected override void OnChecked(RoutedEventArgs e)
+		{
+			ellipse = this.Template.FindName("ellipse", this) as Ellipse;
+			rectangle = this.Template.FindName("rectangleWhite", this) as Path;
+			base.OnChecked(e);
+			if (sbMoveToRight != null)
+			{
+				sbMoveToRight.Begin(ellipse);
+			}
+			if (sbRectangleHide != null)
+			{
+				sbRectangleHide.Begin(rectangle,true);
+			}
+		}
+
+		protected override void OnUnchecked(RoutedEventArgs e)
+		{
+			base.OnUnchecked(e);
+			if (sbMoveToLeft != null)
+			{
+				sbMoveToLeft.Begin(ellipse);
+				if (sbRectangleShow != null)
+				{
+					sbRectangleShow.Begin(rectangle, true);
+				}
+			}
+		}
+	}
+}
