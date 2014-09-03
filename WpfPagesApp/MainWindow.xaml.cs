abf02349ba30e91db391b7022c43d1ac67da2565
ZoeCheck <893622396@qq.com>
2014-09-03 09:04:50 +0800

    添加了网上载的一个WpfPagesApp仿IPhone翻页效果的demo

---
+++ WpfPagesApp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfPagesApp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+
+        private Color GetRandomColor()
+        {
+            return Color.FromRgb((byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int num = int.Parse(textBox1.Text);
+            List<Panel> Panels = new List<Panel>();
+            for (int i = 0; i < num; i++)
+            {
+                WrapPanel rectangle = new WrapPanel();
+                rectangle.Width = tPageControl1.GetPageWidth();
+                rectangle.Height = tPageControl1.GetPageHieght();
+                rectangle.Background = new SolidColorBrush(GetRandomColor());
+                Panels.Add(rectangle);
+            }
+            tPageControl1.AddPage(Panels, 1);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tPageControl1.ClearPage();
+        }
+    }
+}
