dc1f789c9cdb9b7ee117c85d227c3ec414d1f6a8
ZoeCheck <893622396@qq.com>
2014-10-08 13:58:33 +0800

    圆形ProgressBar样式制作

---
+++ CircleProgressBar/CircleProgressBarStyle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace CircleProgressBar
+{
+	/// <summary>
+	/// 作者：xxh 
+	/// 时间：2014-10-08 13:30:07
+	/// 版本：V1.0.0 	 
+	/// </summary>
+	public class CircleProgressBarStyle : ProgressBar
+	{
+		TextBlock tblPercent;
+
+		public CircleProgressBarStyle()
+		{
+			this.Style = this.FindResource("ProgressBarStyleCircle") as Style;
+		}
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+			tblPercent = this.Template.FindName("tblPercent", this) as TextBlock;
+		}
+
+		protected override void OnValueChanged(double oldValue, double newValue)
+		{
+			base.OnValueChanged(oldValue, newValue);
+			tblPercent.Text = string.Format("{0}%", Math.Round(Value / Maximum, 2) * 100);
+		}
+	}
+}
---
+++ CircleProgressBar/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+
+namespace CircleProgressBar
+{
+	/// <summary>
+	/// MainWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		Timer timer;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			timer = new Timer();
+			timer.Interval = 10;
+			timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+		}
+
+		void timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			this.Dispatcher.Invoke(new Action(() =>
+			{
+				//pbMain.Value += 1;
+				c1.Value += 1;
+			}));
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			timer.Start();
+		}
+	}
+}
